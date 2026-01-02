e5941ad5700d9c1ace72926d7f78663366925d50
Joseph <Jasephname12345@gmail.com>
2026-01-02 15:49:42 +0545

    .

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,5 +2,5 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const PORT:number = process.env.PORT? parseInt(process.env.PORT):5000;
-export const DB_URL : string = process.env.MONGO_URI||"mongodb+srv://lamadipen9953_db_user:PxVCD0spiXAwni8Z@cluster0.zb3nmg0.mongodb.net/development";
+export const DB_URL : string = process.env.MONGO_URI||"mongodb+srv://jamlingtamang9953_db_user:PxVCD0spiXAwni8Z@cluster0.zb3nmg0.mongodb.net/development";
 export const JWT_SECRET: string = process.env.JWT_SECRET||"myjwtsecret";
